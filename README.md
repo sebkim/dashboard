7b87c4d35bfbe4f8240c2cd439f9943ac2dfe2dd
Sebo Kim <sanari85@gmail.com>
2016-06-12 12:17:06 -0400

    data component complete

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Dashboard frontend
 
-This project ...
+This app is a corporate dashboard. It reads json format data file. JSON file includes geospatial info, key metric info, and issue data. Geospatial view uses google map API. When a user click company name, the app opens the info window for that company. Key Metric view shows a line chart, and a bar chart. Data view shows all open and closed issues. In this view, each column is sortable. Also, the app provides filter function for showing whether only open or close or both.
+
+It is developed under Version 53.0.2751.0 canary (64-bit).
 
 ## Usage
 
--- dev/data.component.ts
+++ dev/data.component.ts
@@ -1,14 +1,102 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'my-data',
   directives: [],
   // templateUrl: 'templates/app.tpl.html'
   template: `
-    <p>data</p>
+    <h2>Issue Data</h2>
+    <hr>
+    <p style="font-size: 1.5em; font-weight: bold;">Click a column to sort.</p>
+    <div id="mydata-btngroup">
+      <button class="btn btn-primary" (click)="onClickFilter(0)">Filter only open</button>
+      <button class="btn btn-primary" (click)="onClickFilter(1)">Filter only close</button>
+      <button class="btn btn-primary" (click)="onClickFilter(2)">Filter both</button>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-md-6">
+        <div id="myTable"></div>
+      </div>
+    </div>
   `,
 
 })
-export class DataComponent {
+export class DataComponent implements OnInit {
+  dataInfoJson: any = [];
+  tableContainer: any;
+  hot: any;
+  hereObjList: Array<any>;
+  hereObjListOriginal: Array<any>;
+  hotSetting: any;
+
+  constructor(private _http: Http) { }
+
+  ngOnInit(): any {
+    // read datainfo json.
+    this._http.get('json/dataInfo.json')
+      .map((res:Response) => res.json())
+      .subscribe(
+        data => { this.dataInfoJson = data},
+        err => console.error(err),
+        () => {
+          this.hereObjList=[];
+          this.hereObjListOriginal=[];
+          for(let i=0;i<this.dataInfoJson.length;i++) {
+            let hereObj={}
+            hereObj['open'] = this.dataInfoJson[i]['open'];
+            hereObj['content'] = this.dataInfoJson[i]['content'];
+            hereObj['period'] = this.dataInfoJson[i]['period'];
+            let periodSplitted = hereObj['period'].split(' ');
+            hereObj['period'] = periodSplitted[0] + ' / ' + periodSplitted[1];
+            this.hereObjList.push(hereObj);
+            this.hereObjListOriginal.push(Object.assign({}, hereObj));
+          }
+          this.tableContainer = document.getElementById('myTable');
+          this.hotSetting = {
+            readOnly: true,
+            stretchH: 'all',
+            data: this.hereObjList,
+            colHeaders: ['Open or Close', 'Content', 'Year / Month'],
+            rowHeaders: true,
+            columnSorting: true,
+            sortIndicator: true
+          };
+          this.hot = new Handsontable(this.tableContainer,this.hotSetting);
+        } // end of ()=> {}
+      );
+
+  }
+
+  onClickFilter(option: number):any {
+    if(option===0) {
+      this.hereObjList=this.hereObjListOriginal.filter(this.isitOpen);
+    } else if (option===1) {
+      this.hereObjList=this.hereObjListOriginal.filter(this.isitClose);
+    } else {
+      this.hereObjList=this.hereObjListOriginal;
+    }
+    this.hotSetting['data']=this.hereObjList;
+    this.hot.updateSettings(this.hotSetting);
+    this.hot.render();
+  }
+
+  private isitOpenOrClose(option: number, hObj: any): boolean {
+    if(option===0) {
+        if(hObj['open']==='open') {
+          return true;
+        } else return false;
+    }
+    else if (option===1) {
+      if(hObj['open']==='closed') {
+        return true;
+      } else return false;
+    }
+  }
+  private isitOpen = this.isitOpenOrClose.bind(null, 0);
+  private isitClose = this.isitOpenOrClose.bind(null, 1);
 
 }
